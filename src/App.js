302a91f2d7510b46c5c295bbbe439a042543100f
Lawrence Caulker <locaulker@gmail.com>
2023-06-16 18:31:12 -0400

    added basic structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,36 @@
 import React from "react"
 
 export default function App() {
-  return <div>App</div>
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  )
+}
+
+function Logo() {
+  return <h1>🏝 Far Away 👜</h1>
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do need for your 😍 trip?</h3>
+    </div>
+  )
+}
+
+function PackingList() {
+  return <div className="list">LIST</div>
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>💼 You have X items in your list, and you already packed X (X%)</em>
+    </footer>
+  )
 }
