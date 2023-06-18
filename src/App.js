202fb7bda7a408fd90c74c94286b938c87a2339d
Lawrence Caulker <locaulker@gmail.com>
2023-06-18 02:15:03 -0400

    implemented controlled elements

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: true },
@@ -18,14 +18,46 @@ export default function App() {
 }
 
 function Logo() {
-  return <h1>🏝 Far Away 👜</h1>
+  return <h1>🏝 Far Away 🧳</h1>
 }
 
 function Form() {
+  const [description, setDescription] = useState("")
+  const [quantity, setQuantity] = useState(1)
+
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    if (!description) return
+
+    const newItem = { description, quantity, packed: false, id: Date.now() }
+    console.log(newItem)
+
+    setDescription("")
+    setQuantity(1)
+  }
+
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do need for your 😍 trip?</h3>
-    </div>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button>Add</button>
+    </form>
   )
 }
 
@@ -35,7 +67,7 @@ function PackingList() {
     <div className="list">
       <ul>
         {initialItems.map((item) => (
-          <Item item={item} />
+          <Item item={item} key={item.id} />
         ))}
       </ul>
     </div>
@@ -57,7 +89,7 @@ function Item({ item }) {
 function Stats() {
   return (
     <footer className="stats">
-      <em>💼 You have X items in your list, and you already packed X (X%)</em>
+      <em>💼 You have X items in your list, and you already packed X (x%)</em>
     </footer>
   )
 }
