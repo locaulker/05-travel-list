59f4d80a1e0527af23f271a2c0a36e289e82b36f
Lawrence Caulker <locaulker@gmail.com>
2023-06-24 05:12:31 -0400

    implemented child-to-parent delete operation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import React, { useState } from "react"
 
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: true },
-  { id: 2, description: "Socks", quantity: 12, packed: true },
-  { id: 3, description: "Charger", quantity: 1, packed: false }
-]
-
 export default function App() {
   const [items, setItems] = useState([])
 
   function handleAddItems(item) {
-    setItems(items => [...items, item])
+    setItems((items) => [...items, item])
+  }
+
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id))
   }
 
   return (
     <div className="app">
       <Logo />
       <Form onAddItems={handleAddItems} />
-      <PackingList items={items} />
+      <PackingList items={items} onDeleteItem={handleDeleteItem} />
       <Stats />
     </div>
   )
@@ -50,9 +48,9 @@ function Form({ onAddItems }) {
       <h3>What do need for your 😍 trip?</h3>
       <select
         value={quantity}
-        onChange={e => setQuantity(Number(e.target.value))}
+        onChange={(e) => setQuantity(Number(e.target.value))}
       >
-        {Array.from({ length: 20 }, (_, i) => i + 1).map(num => (
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
           </option>
@@ -62,7 +60,7 @@ function Form({ onAddItems }) {
         type="text"
         placeholder="Item..."
         value={description}
-        onChange={e => setDescription(e.target.value)}
+        onChange={(e) => setDescription(e.target.value)}
       />
       <button>Add</button>
     </form>
@@ -70,12 +68,12 @@ function Form({ onAddItems }) {
 }
 
 // Making a list of Items(Components) for each array element
-function PackingList({ items }) {
+function PackingList({ items, onDeleteItem }) {
   return (
     <div className="list">
       <ul>
-        {items.map(item => (
-          <Item item={item} key={item.id} />
+        {items.map((item) => (
+          <Item item={item} onDeleteItem={onDeleteItem} key={item.id} />
         ))}
       </ul>
     </div>
@@ -83,13 +81,13 @@ function PackingList({ items }) {
 }
 
 // Item component generated from each iteration of the map() function
-function Item({ item }) {
+function Item({ item, onDeleteItem }) {
   return (
     <li>
       <span style={item.packed ? { textDecoration: "line-through" } : {}}>
         {item.quantity} {item.description}
       </span>
-      <button>❌</button>
+      <button onClick={() => onDeleteItem(item.id)}>❌</button>
     </li>
   )
 }
